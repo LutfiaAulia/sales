1a8b97f3383da20f44966b8a99125d9bb1c315bc
LutfiaAulia <lutfiaaulia2@gmail.com>
2024-01-25 14:58:16 +0700

    hehehehe

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,39 +1,43 @@
-const {Sequelize, DataType, DataTypes} = require ("sequelize");
-const sequelize = require ("../config/database");
+const { Sequelize, DataType, DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
 
 const User = sequelize.define(
-    "User",
-    {
-        idUser:{
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        userName:{
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        nama: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        email:{
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        password:{
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        role:{
-            type: DataTypes.ENUM,
-            values: [Admin, Karyawan],
-            allowNull: false,
-        },
+  "User",
+  {
+    idUser: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        tableName: "users",
-        timestamps: true,
-    }
+    idSektor: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    nama: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM,
+      values: [Admin, Karyawan],
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "users",
+    timestamps: true,
+  }
 );
 module.exports = User;
